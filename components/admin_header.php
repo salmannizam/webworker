3ec6976859097d8ddb104d575dc3de8dd27f04fe
salman nizam <101084548+salmannizam@users.noreply.github.com>
2025-08-31 18:45:32 +0530

    Update admin_header.php

--- components/admin_header.php
+++ components/admin_header.php
@@ -1,10 +1,8 @@
 <?php
-// Start session if not already started
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Check login
 if (!($_SESSION["logged_in"] ?? false)) {
     header("Location: /login.php");
     exit;
